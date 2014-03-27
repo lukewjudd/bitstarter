fcc33c1d07c0fe4ccae002af882c4abe8f9567ae
Luke Judd <lukewjudd@gmail.com>
2014-03-27 04:34:34 +0000

    Index added

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+ var buf  = fs.readFileSync('index.html');
+ response.send(buf.toString());
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
